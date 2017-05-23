b1328956ea24dd20c0193be3fd5a001d28dd2210
Kelsey Kerr <kerr.kelsey@gmail.com>
2017-05-23 13:23:03 -0400

    + add terms text to login page and send ip/tos accepted automatically on first login

--- Lend/LoginViewController.swift
+++ Lend/LoginViewController.swift
@@ -12,9 +12,11 @@ protocol LoginViewDelegate: class {
     func didTapLoginButton()
 }
 
-class LoginViewController: UIViewController, GIDSignInDelegate, GIDSignInUIDelegate {
+class LoginViewController: UIViewController, GIDSignInDelegate, GIDSignInUIDelegate, UITextViewDelegate {
 
     weak var delegate: LoginViewDelegate?
+    
+    @IBOutlet var termsText: UITextView!
     
     @IBAction func tappedGoogleLogin() {
         tappedLoginButton(type: .google)
@@ -26,6 +28,34 @@ class LoginViewController: UIViewController, GIDSignInDelegate, GIDSignInUIDeleg
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let termsLinkAttributes = [
+            NSLinkAttributeName: NSURL(string: "http://thenearbyapp.com/terms")!,
+            NSUnderlineStyleAttributeName: NSUnderlineStyle.styleSingle.rawValue,
+            NSForegroundColorAttributeName: UIColor.white
+            ] as [String : Any]
+        
+        let privacyLinkAttributes = [
+            NSLinkAttributeName: NSURL(string: "http://thenearbyapp.com/privacy")!,
+            NSUnderlineStyleAttributeName: NSUnderlineStyle.styleSingle.rawValue,
+            NSForegroundColorAttributeName: UIColor.white
+            ] as [String : Any]
+        
+        let linkAttributes: [String : Any] = [
+            NSForegroundColorAttributeName: UIColor.white,
+            NSUnderlineColorAttributeName: UIColor.white,
+            NSUnderlineStyleAttributeName: NSUnderlineStyle.styleSingle.rawValue]
+
+        
+        let allAttributes = [NSForegroundColorAttributeName: UIColor.white] as [String : Any]
+        
+        let attributedString = NSMutableAttributedString(string: "By signing up, I agree to Nearby's Terms of Service, End User License Agreement, and Privacy Policy")
+        attributedString.setAttributes(allAttributes, range: NSMakeRange(0, 34))
+        attributedString.setAttributes(termsLinkAttributes, range: NSMakeRange(35, 44))
+        attributedString.setAttributes(allAttributes, range: NSMakeRange(79, 5))
+        attributedString.setAttributes(privacyLinkAttributes, range: NSMakeRange(85, 14))
+        self.termsText.linkTextAttributes = linkAttributes
+        self.termsText.delegate = self
+        self.termsText.attributedText = attributedString
         
         GIDSignIn.sharedInstance().delegate = self
         GIDSignIn.sharedInstance().uiDelegate = self
@@ -51,6 +81,11 @@ class LoginViewController: UIViewController, GIDSignInDelegate, GIDSignInUIDeleg
     
     func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) {
         
+    }
+
+    @available(iOS 10.0, *)
+    func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
+        return true
     }
 
 }
